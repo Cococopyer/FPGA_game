0680be296fd60ec06f3bc41961f63954446a1626
Cococopyer <68847082+Cococopyer@users.noreply.github.com>
2020-08-02 19:45:56 +0800

    Add files via upload

---
+++ ip/PICTURE_R_ROM/PICTURE_R_ROM_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+// Date        : Sat Aug  1 20:11:23 2020
+// Host        : USER-King running 64-bit Service Pack 1  (build 7601)
+// Command     : write_verilog -force -mode synth_stub
+//               E:/Xilinx/Bird/Bird.srcs/sources_1/ip/PICTURE_R_ROM/PICTURE_R_ROM_stub.v
+// Design      : PICTURE_R_ROM
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7s15ftgb196-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_2,Vivado 2018.3" *)
+module PICTURE_R_ROM(clka, ena, wea, addra, dina, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,ena,wea[0:0],addra[13:0],dina[7:0],douta[7:0]" */;
+  input clka;
+  input ena;
+  input [0:0]wea;
+  input [13:0]addra;
+  input [7:0]dina;
+  output [7:0]douta;
+endmodule
---
+++ ip/clk_wiz_0/clk_wiz_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+// Date        : Thu Jul 30 14:37:43 2020
+// Host        : USER-King running 64-bit Service Pack 1  (build 7601)
+// Command     : write_verilog -force -mode synth_stub e:/Xilinx/Bird/Bird.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7s15ftgb196-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module clk_wiz_0(clk_out1, reset, locked, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,reset,locked,clk_in1" */;
+  output clk_out1;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
